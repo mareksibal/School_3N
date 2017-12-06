c277217e675e6708a0e562e9ec07deb9207c9dab
Marek <marek.sibal@akademiasovy.sk>
2017-12-06 14:26:19 +0100

    Refactoring of script file, added validators. Updating table with new Person on click.

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
 $(document).ready(function() {
    $("#btnOk").click(function(event) {
 		var fname=$("#fname").val();
+		var mozemPridatRiadok = true;
 		if(fname.trim().length<2){
 			console.log("menej ako 2");
 			$("#errfname").html("Invalid first name.");
-		} else {
-			$('body > div.table > table > tbody > tr:nth-child(2) > td:nth-child(1)').html( $('#fname').val() );
+			mozemPridatRiadok = false;
 		}
 		
 		var lname=$("#lname").val();
 		if(lname.trim().length<2){
 			$("#errlname").html("Invalid last name.");
-		} else {
-			$('body > div.table > table > tbody > tr:nth-child(2) > td:nth-child(2)').html( $('#lname').val() );
+			mozemPridatRiadok = false;
 		}
 		 
 		var date = new Date($('#birthdate').val());
 		var newdate = new Date();
 		
-		$('body > div.table > table > tbody > tr:nth-child(2) > td:nth-child(3)').html( newdate.getFullYear() - date.getFullYear() );		
+		if (isNaN(parseFloat(newdate.getFullYear() - date.getFullYear())) || !(newdate.getFullYear() > date.getFullYear())) {
+				$("#errdate").html("Invalid date.");
+				mozemPridatRiadok = false;
+		}
+
+		if (mozemPridatRiadok) {
+			$( "#PersonInfoTable" ).append( "<tr><td>" + $('#fname').val() + "</td><td>" + $('#lname').val() + "</td><td>" + (newdate.getFullYear() - date.getFullYear()).toString() + "</td></tr>	" );		
+			$("#fname").val("");
+			$("#lname").val("");
+			$("#birthdate").val("mm/dd/yyyy");
+			$("#errdate").html("");
+		}
+	
    });
 
-   /*
+   
    $("#fname").keyup(function(event) {
      console.log("sss");
      var fname=$("#fname").val();
@@ -37,5 +48,5 @@ $(document).ready(function() {
        $("#errlname").html("");
      }
    });
-   */
+   
 });
